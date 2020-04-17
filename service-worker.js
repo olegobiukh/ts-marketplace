817da90c55b4cc94b12321b6745fa10316ed0536
Oleg Obiukh 2020 <oleg.obiuh.subscribe@gmail.com>
2020-04-18 01:11:36 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ts-marketplace/precache-manifest.2a59faca17235a271106fe0349e5368f.js"
+  "/ts-marketplace/precache-manifest.613899520bf3735e31c78f7949d35bd3.js"
 );
 
 self.addEventListener('message', (event) => {
