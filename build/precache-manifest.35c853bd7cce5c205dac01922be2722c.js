0d194af8097ba112e2b859c4d75ad0eae0f184d9
Oleg Obiukh 2020 <oleg.obiuh.subscribe@gmail.com>
2020-04-17 11:03:21 +0300

    Deploy 1

---
+++ build/precache-manifest.35c853bd7cce5c205dac01922be2722c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7edc2ac9ffaddce9059512929327f4",
+    "url": "/ts-marketplace/build/index.html"
+  },
+  {
+    "revision": "f29e0433c5c571d9516f",
+    "url": "/ts-marketplace/build/static/css/main.c3f1b29e.chunk.css"
+  },
+  {
+    "revision": "a7dd7187c46b0b33a2c7",
+    "url": "/ts-marketplace/build/static/js/2.851d477d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ts-marketplace/build/static/js/2.851d477d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29e0433c5c571d9516f",
+    "url": "/ts-marketplace/build/static/js/main.bea4270d.chunk.js"
+  },
+  {
+    "revision": "18b1970bd92a1aaa9ad5",
+    "url": "/ts-marketplace/build/static/js/runtime-main.954dbd8a.js"
+  }
+]);
