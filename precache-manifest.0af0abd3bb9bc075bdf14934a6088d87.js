939984b07dc1bebbde74723e745a82216718fa29
Oleg Obiukh 2020 <oleg.obiuh.subscribe@gmail.com>
2020-04-28 11:46:20 +0300

    Updates

---
+++ precache-manifest.0af0abd3bb9bc075bdf14934a6088d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87c875748ef9fd44c98ab101894cbb3",
+    "url": "/ts-marketplace/index.html"
+  },
+  {
+    "revision": "340544847f01c6bf2707",
+    "url": "/ts-marketplace/static/css/main.5d5545e6.chunk.css"
+  },
+  {
+    "revision": "242bf159a86713c2b777",
+    "url": "/ts-marketplace/static/js/2.1bf50f65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ts-marketplace/static/js/2.1bf50f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340544847f01c6bf2707",
+    "url": "/ts-marketplace/static/js/main.384cbb18.chunk.js"
+  },
+  {
+    "revision": "c0b422a844799fb753af",
+    "url": "/ts-marketplace/static/js/runtime-main.33fa5bd7.js"
+  },
+  {
+    "revision": "d54bca199c7a0ebc5d8ae62e362622df",
+    "url": "/ts-marketplace/static/media/logo.d54bca19.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ts-marketplace/precache-manifest.aaf7fe1535d685e7a41a2fabc5ba37df.js"
+  "/ts-marketplace/precache-manifest.0af0abd3bb9bc075bdf14934a6088d87.js"
 );
 
 self.addEventListener('message', (event) => {
